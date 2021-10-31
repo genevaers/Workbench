6fa464d4bf5cb0ec0bf6c306753516750f3e216a
hammyau <blueham60@gmail.com>
2021-10-31 17:28:44 +0800

    store fields by name
    Signed-off-by: hammyau <blueham60@gmail.com>

--- ccb2lr/src/main/java/org/genevaers/ccb2lr/RecordModel.java
+++ ccb2lr/src/main/java/org/genevaers/ccb2lr/RecordModel.java
@@ -1,14 +1,13 @@
 package org.genevaers.ccb2lr;
 
-import java.io.ObjectInputStream.GetField;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
+import java.util.LinkedHashMap;
 
 public class RecordModel {
     
 	private String name;
-	private List<CobolField> fields = new ArrayList<>();
+	private LinkedHashMap<String, CobolField> fields = new LinkedHashMap<>();
 
 
 	public String getName() {
@@ -19,25 +18,29 @@ public class RecordModel {
         this.name = name;
     }
 
-	public List<CobolField> getFields() {
-		return fields;
+	public Collection<CobolField> getFields() {
+		return fields.values();
 	}
 
     public void addField(CobolField f) {
-        fields.add(f);
+        fields.put(f.getName(), f);
     }
 
     public Iterator<CobolField> getFieldIterator() {
-        return fields.iterator();
+        return fields.values().iterator();
     }
 
     public int getLength() {
         int length = 0;
-        Iterator<CobolField> fit = fields.iterator();
+        Iterator<CobolField> fit = fields.values().iterator();
         while(fit.hasNext()) {
             CobolField cbf = fit.next();
             length += cbf.getLength();
         }
         return length;
+    }
+
+    public CobolField getField(String name) {
+        return fields.get(name);
     }
 }
