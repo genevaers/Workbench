e6d95fbcfd178198032b5281110f546e52ecc4ca
bobmcc9 <bobmcc@au1.ibm.com>
2023-03-31 15:05:38 +0800

    remove apache 2.0 wrongly added to parts with a EPL license already
    Signed-off-by: bobmcc9 <bobmcc@au1.ibm.com>

--- plugins/nebulagrid/src/org/eclipse/nebula/widgets/grid/internal/DefaultFocusRenderer.java
+++ plugins/nebulagrid/src/org/eclipse/nebula/widgets/grid/internal/DefaultFocusRenderer.java
@@ -10,24 +10,6 @@
  *******************************************************************************/ 
 package org.eclipse.nebula.widgets.grid.internal;
 
-/*
- * Copyright Contributors to the GenevaERS Project. SPDX-License-Identifier: Apache-2.0 (c) Copyright IBM Corporation 2008.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-
 import org.eclipse.nebula.widgets.grid.AbstractRenderer;
 import org.eclipse.nebula.widgets.grid.GridItem;
 import org.eclipse.swt.SWT;
