008aa48f8c7beae252b2799270ab0434583e79d4
hammyau <blueham60@gmail.com>
2024-06-20 14:51:25 +0800

    runworkbench restore
    Signed-off-by: hammyau <blueham60@gmail.com>

--- runWorkbench.sh
+++ runWorkbench.sh
@@ -13,4 +13,5 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-./products/com.ibm.safr.we.product/target/products/wb/win32/win32/x86_64/GenevaERS.exe
+cd products/com.ibm.safr.we.product/target/products/wb/win32/win32/x86_64/ 
+./GenevaERS.exe
