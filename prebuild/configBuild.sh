0408f3946023444e05d8bb8fb36e5ff0de0cabea
hammyau <blueham60@gmail.com>
2024-09-18 11:43:38 +0800

    bump to RC5 and get rcapps
    Signed-off-by: hammyau <blueham60@gmail.com>

--- prebuild/configBuild.sh
+++ prebuild/configBuild.sh
@@ -33,5 +33,5 @@ else
     echo "*******************************"
 fi 
 echo "Copy the appchooser"
-cp ../Run-Control-Apps/RCApps/target/rcapps-1.1.0_RC4-jar-with-dependencies.jar $GENEVAGUI/lib/
+cp ../Run-Control-Apps/RCApps/target/rcapps-1.1.0_RC5-jar-with-dependencies.jar $GENEVAGUI/lib/
 cat prebuild/build.properties.end >> $GENEVAGUI/build.properties
