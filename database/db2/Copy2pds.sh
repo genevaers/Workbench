7751bba7e34d851335d6dbfe958e458cc87c770f
neilbeesley <neil.beesley@ibm.com>
2025-09-26 13:13:20 -0500

    scripting
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/Copy2pds.sh
+++ database/db2/Copy2pds.sh
@@ -39,7 +39,7 @@ while IFS= read -r line; do
 
   if [ $staidx -gt 0 ] && [ $endidx -gt $staidx ]; then
 #    file=$(expr substr "$line" 1 $((endidx-1)) );
-    file=$(expr substr "$line" $staidx );
+    file=$(expr substr( "$line" $staidx ));
     echo "Preparing file: $file";
     # cp -S d=."$FROM_SUF" "$file"."$FROM_SUF" "$TO_PDS";
     # cp -S d=."$FROM_SUF" "$FROM_DIR"/*."$FROM_SUF" "$TO_PDS";
