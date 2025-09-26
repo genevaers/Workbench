a7ce48551ed38de874c617998c92ec0126ea5739
neilbeesley <neil.beesley@ibm.com>
2025-09-26 18:02:12 -0500

    script
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/prepare_ddl.sh
+++ database/db2/prepare_ddl.sh
@@ -27,12 +27,16 @@ mycmdstr3='s/&$DBSG.'/${GERS_DBSG}/'g'
 mycmdstr4='s/&$DBSUB.'/${GERS_DBSUB}/'g'
 mycmdstr5='s/&$DBSCH.'/${GERS_DBSCH}/'g'
 
-# perform substitutions
-sed -W filecodeset=IBM-1047 $mycmdstr1   $MEMBER > prep/tmp1;
-sed -W filecodeset=IBM-1047 $mycmdstr2 prep/tmp1 > prep/tmp2;
-sed -W filecodeset=IBM-1047 $mycmdstr3 prep/tmp2 > prep/tmp3;
-sed -W filecodeset=IBM-1047 $mycmdstr4 prep/tmp3 > prep/tmp4;
-sed -W filecodeset=IBM-1047 $mycmdstr5 prep/tmp4 > prep/$MEMBER;
+# perform substitutions which unfortunately still converts to ACII with -W filecodeset=IBM-1047 
+sed $mycmdstr1   $MEMBER > prep/tmp1;
+sed $mycmdstr2 prep/tmp1 > prep/tmp2;
+sed $mycmdstr3 prep/tmp2 > prep/tmp3;
+sed $mycmdstr4 prep/tmp3 > prep/tmp4;
+sed $mycmdstr5 prep/tmp4 > prep/tmp5;
+
+#convert back to EBCDIC again
+iconv -f ISO8859-1 -t IBM-1047 prep/tmp5 > prep/$MEMBER;
+chtag -r prep/$MEMBER;
 
 }
 
