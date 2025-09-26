0377df0e09dcf37e6093595ccd2206ab2a338166
neilbeesley <neil.beesley@ibm.com>
2025-09-26 15:22:01 -0500

    Scripting
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/Copy2pds.sh
+++ database/db2/Copy2pds.sh
@@ -1,50 +1,69 @@
 #!/bin/bash
 # Script to directory contents to MVS pds(e)
 
+main() {
+
+# Variables chosen by user
+export GERS_DBUSER=SAFRBLD
+export GERS_DBNAME=SADBNEIL
+export GERS_DBSG=SASGNEIL
+export GERS_DBSCH=SAFRNEIL
+export GERS_DBSUB=DM13
+
 # Check if a directory and pattern are provided
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo "Usage: $0 <directory> <suffix> <pds>"
-  echo "Example: $0 /u/usr1/sample  jcl  //'SMPL.JCL'"
-  exit 1
+  echo "Usage: $0 <directory> <suffix> <pds>";
+  echo "Example: $0 /u/usr1/sample  jcl  //'SMPL.JCL'";
+  exit 1;
 fi
 
 FROM_DIR="$1";
 FROM_SUF="$2";
 TO_PDS="$3";
 
-echo "Preparing files in directory: $FROM_DIR with suffix: $FROM_SUF and copying to MVS dataset: $TO_PDS"
+echo "Preparing files from directory: $FROM_DIR with suffix: $FROM_SUF and copying to MVS dataset: $TO_PDS"
+
+# Remove data preparation directory and create fresh
+rm -Rf prep;
+mkdir prep;
 
 # Determine directory contents
-
 # echo "$FROM_DIR"/*."$FROM_SUF"
 
 ls "$FROM_DIR"/*."$FROM_SUF" > "$FROM_DIR"/list.tmp
-
 FILE="$FROM_DIR/list.tmp"; # File to parse to get directory contents
 if [ ! -f "$FILE" ]; then
   echo "Error: Temporary file '$FILE' not found.";
   exit 1;
 fi
 
+# Process each file that matches the pattern
 while IFS= read -r line; do
-  # Process each line (record) here
-  # echo "Record: $line";
-  # TO_PDS=""""$TO_PDS"""";
-  # echo "TO_PDS: $TO_PDS";
-  # Find last index of test we're searching for
   echo $line > "$FROM_DIR"/text.tmp;
   staidx=$(awk -F"/" '{print length($0) - length($NF)}' "$FROM_DIR"/"text.tmp" );
   endidx=$(awk -F"." '{print length($0) - length($NF)}' "$FROM_DIR"/"text.tmp" );
   echo "Staidx: $staidx Endidx: $endidx Line: $line";
 
   if [ $staidx -gt 0 ] && [ $endidx -gt $staidx ]; then
-#    file=$(expr substr "$line" 1 $((endidx-1)) );
-#    file=$(expr substr "$line" $((staidx+1)) );
     file="${line:$staidx}"
     echo "Preparing file: $file";
-    # cp -S d=."$FROM_SUF" "$file"."$FROM_SUF" "$TO_PDS";
-    # cp -S d=."$FROM_SUF" "$FROM_DIR"/*."$FROM_SUF" "$TO_PDS";
-
+    . ./prepare_ddl.sh "$file";
   fi
 
 done < "$FILE"
+
+# Copy the processed files from preparation directory to MVS PDSE
+    # cp -S d=."$FROM_SUF" "$file"."$FROM_SUF" "$TO_PDS";
+cp -S d=."$FROM_SUF" "$FROM_DIR"/prep/*."$FROM_SUF" "$TO_PDS";
+
+}
+
+exitIfError() {
+
+if [ $? != 0 ]
+then
+    echo "$(date) ${BASH_SOURCE##*/} *** Process terminated: see error message above";
+    exit 1;
+fi
+
+}
--- database/db2/prepare_ddl.sh
+++ database/db2/prepare_ddl.sh
@@ -1,9 +1,42 @@
 #!/bin/sh
+# prepare_ddl.sh - Prepare data management statements for user preferences
+########################################################
 
-member = "GVBQDRAL.DDL";
-sed 's/&$DBUSER./SAFRBLD/g' $member > tmp1
-sed 's/&$DBNAME./SADBNEIL/g'   tmp1 > tmp2
-sed 's/&$DBSG./SASGNEIL/g'     tmp2 > tmp3
-sed 's/&$DBSCH./SAFRNEIL/g'    tmp3 > prep/$member
+main() {
 
-# sed 's/&$DBUSER./SAFRBLD/g' GVBQDRAL.DDL > gvbqdral.ddl
+# &$DBUSER.   DATABASE USER
+# &$DBNAME.   DATABASE NAME
+# &$DBSG.     DATABASE STORAGE GROUP
+# &$DBSCH.    DATABASE SCHEMA
+# &$DBSUB.    DATABASE SUBSYSTEM
+
+# Check if member name is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <name of file to prepare";
+  echo "Example: $0 GVBQDRAL.DDL";
+  exit 1;
+fi
+
+MEMBER="$1";
+# member = "GVBQDRAL.DDL";
+
+# never mind the symbolics
+sed 's/&$DBUSER./"GERS_DBUSER"/g'    $MEMBER > prep/tmp1;
+sed 's/&$DBNAME./"$GERS_DBNAME"/g' prep/tmp1 > prep/tmp2;
+sed 's/&$DBSG./"$GERS_DBSG"/g'     prep/tmp2 > prep/tmp3;
+sed 's/&$DBSUB./"$GERS_DBSUB"/g'   prep/tmp3 > prep/tmp4;
+sed 's/&$DBSCH./"$GERS_DBSCH"/g'   prep/tmp4 > prep/$MEMBER;
+
+}
+
+exitIfError() {
+
+if [ $? != 0 ]
+then
+    echo "$(date) ${BASH_SOURCE##*/} *** Process terminated: see error message above";
+    exit 1;
+fi
+
+}
+
+main "$@"
