9d662fb2d6ca9d6c5e0ea3a851b789e7c51f2b58
neilbeesley <neil.beesley@ibm.com>
2025-09-26 13:40:58 -0500

    script
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/prepare_ddl.sh
+++ database/db2/prepare_ddl.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-sed 's/&$DBUSER./SAFRBLD/g' GVBQDRAL.DDL  > tmp1
-sed 's/&$DBNAME./SADBNEIL/g'         tmp1 > tmp2
-sed 's/&$DBSG./SASGNEIL/g'           tmp2 > tmp3
-sed 's/&$DBSCH./SAFRNEIL/g'          tmp3 > gvbqdral.ddl
+member = "GVBQDRAL.DDL";
+sed 's/&$DBUSER./SAFRBLD/g' $member > tmp1
+sed 's/&$DBNAME./SADBNEIL/g'   tmp1 > tmp2
+sed 's/&$DBSG./SASGNEIL/g'     tmp2 > tmp3
+sed 's/&$DBSCH./SAFRNEIL/g'    tmp3 > prep/$member
 
 # sed 's/&$DBUSER./SAFRBLD/g' GVBQDRAL.DDL > gvbqdral.ddl
