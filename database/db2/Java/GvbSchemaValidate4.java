e56648119d7b6876e1fa3be5d7bc89323dea7710
neilbeesley <neil.beesley@ibm.com>
2025-10-27 11:23:55 -0500

    Renaming
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/Java/GvbSchemaValidate4.java
+++ database/db2/Java/GvbSchemaValidate4.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 
 import java.sql.*;
  
-public class Db2test5 {
+public class GvbSchemaValidate4 {
     public static void main(String[] args)
     {
         HashMap<String, String> spmap = new HashMap<>(100);
