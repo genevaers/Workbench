8fc0ef34aa3c247ee2c7564626146f6d91a0f779
AswiniV <aswini.vadlamudi@ibm.com>
2026-01-20 12:10:05 -0600

    updated to RCA RC21
    Signed-off-by: AswiniV <aswini.vadlamudi@ibm.com>

--- prebuild/configBuild.sh
+++ prebuild/configBuild.sh
@@ -33,5 +33,5 @@ else
     echo "*******************************"
 fi 
 echo "Copy the appchooser"
-cp ../Run-Control-Apps/RCApps/target/rcapps-1.1.0_RC20-jar-with-dependencies.jar $GENEVAGUI/lib/
+cp ../Run-Control-Apps/RCApps/target/rcapps-1.1.0_RC21-jar-with-dependencies.jar $GENEVAGUI/lib/
 cat prebuild/build.properties.end >> $GENEVAGUI/build.properties
