0dfc908a8d3877683f894aa78dd732f23601d281
neilbeesley <neil.beesley@ibm.com>
2026-01-27 22:17:17 -0600

    Don't lose the prototypes this time
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/test/test3.sh
+++ database/db2/test/test3.sh
@@ -19,7 +19,7 @@ while IFS= read -r line; do
       echo "RUN PROGRAM(DSNTEP2) PLAN(&DB2PLAN) - " >> "$DEST_FILE";    
     fi
   else
-    if [[ "$lastline" != "" && "$lastline" != *"LIB('&DB2RLIB.')"* ]]; then
+    if [[ "$lastline" != "" && "$lastline" != *"DB2RLIB"* ]]; then
       echo "$lastline" >> "$DEST_FILE";
     fi
   fi
