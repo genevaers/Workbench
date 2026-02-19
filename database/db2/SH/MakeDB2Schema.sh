d5b3c68910de7906e352e2c699885fc8eeafdb51
neilbeesley <neil.beesley@ibm.com>
2026-02-18 23:37:29 -0600

    fix
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/SH/MakeDB2Schema.sh
+++ database/db2/SH/MakeDB2Schema.sh
@@ -26,9 +26,11 @@ fi
 
 # determine if user wants to explicitly mentiom DB2 RUNLIB.LOAD in JCL, etc.
 
-if [[ -z "$GERS_DB2_RUN_LIB" || $GERS_DB2_RUN_LIB == "" ]]; then
+if [[ -z "$GERS_DB2_RUN_LIB" || "$GERS_DB2_RUN_LIB" == "" ]]; then
   echo "All references to DB2 RUN library in JCL will be suppressed";
   export GERS_INCLUDE_DB2_RUNLIB=N;
+else
+  export GERS_INCLUDE_DB2_RUNLIB=Y;  
 fi
 
 # determine name of MVS dataset to contain DDL's
