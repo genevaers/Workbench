02f7b371100d509f11ff545264734e666f8e5d82
neilbeesley <neil.beesley@ibm.com>
2026-03-03 13:19:10 -0600

    log files
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/SH/editRLIB.sh
+++ database/db2/SH/editRLIB.sh
@@ -41,7 +41,7 @@ while IFS= read -r line; do
       if [[ "$pre" != "DSN=" || $len > 27 ]]; then
         echo "$lastline" >> "$DEST_FILE";
       else
-        echo "Skipping Include for DB2 Run Library";
+        echo "Skipping Include for DB2 Run Library | tee -a $out_log";
       fi
     fi
   fi
