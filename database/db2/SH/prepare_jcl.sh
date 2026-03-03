e3cc6297cb09034fdc834fde74fbd03b9ec1d7e8
neilbeesley <neil.beesley@ibm.com>
2026-03-03 13:37:44 -0600

    log files
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/SH/prepare_jcl.sh
+++ database/db2/SH/prepare_jcl.sh
@@ -110,7 +110,7 @@ sed $mycmdstr34 ../"$FROM_DIR"/prep/tmp33 > ../"$FROM_DIR"/prep/tmp34;
 
 # Remove DB2RLIB contextualy, if variable not set
 if [[ $GERS_INCLUDE_DB2_RUNLIB == "N" ]]; then
-  echo "Processing file: $MEMBER to remove references to DB2RLIB";
+  echo "Processing file: $MEMBER to remove references to DB2RLIB" | tee -a $out_log;
   ./editRLIB.sh ../"$FROM_DIR"/prep/tmp34 "$FROM_DIR" 2> >(tee -a $err_log) > >(tee -a $out_log);
     exitIfError;
   # echo "File $MEMBER copied from .tmp back to original name with DB2RLIB removed if it was present";
