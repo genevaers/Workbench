30e2dae88bd86ebb190ae24bc228b8dc5028b69d
neilbeesley <neil.beesley@ibm.com>
2026-05-05 23:16:33 -0500

    typo
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/DSNMOD/src/main/java/org/genevaers/dsnmod/DSNMOD.java
+++ database/db2/DSNMOD/src/main/java/org/genevaers/dsnmod/DSNMOD.java
@@ -175,26 +175,27 @@ public class DSNMOD {
     }
 
     public static Integer processPnchFiles(String maskNew, String schemaNameOld, String schemaNameNew ) {
-      CatalogSearch cs = new CatalogSearch("GEBT.GENERS.D251222U.**");
-        cs.addFieldName("ENTNAME"); // Entry Name (Dataset Name)
-        cs.addFieldName("VOLSER");  // Volume Serial
-        cs.addFieldName("DEVTYP");  // Device Type
-        cs.addFieldName("ENTTYPE"); // Entry Type (GDS, PDS, etc.)
-        cs.addFieldName("DSNAME");
+      CatalogSearch cs = new CatalogSearch("GEBT.GENERS.D251222U.**", 64000);
+        //cs.addFieldName("VOLSER");  // Volume Serial
+        //cs.addFieldName("DEVTYP");  // Device Type
+        //cs.addFieldName("ENTTYPE"); // Entry Type (GDS, PDS, etc.)
+        //cs.addFieldName("DSNAME");
 
         // Define search criteria (dataset name, volume, etc.)
         try {
+          cs.addFieldName("ENTNAME"); // Entry Name (Dataset Name)
           cs.search();
           while (cs.hasNext()) {
             CatalogSearch.Entry entry = (CatalogSearch.Entry)cs.next();
             if (entry.isDatasetEntry()) {
-              String dsName = entry.getName();
-              String volser = entry.getField("VOLSER").toString();
-              String devType = entry.getField("DEVTYP").toString();
+              CatalogSearchField field = entry.getField("ENTNAME");
+              String dsName = field.getFString().trim();
+              //String volser = entry.getField("VOLSER").toString();
+              //String devType = entry.getField("DEVTYP").toString();
                     
               System.out.println("Dataset: " + dsName);
-              System.out.println("  VOLSER: " + volser);
-              System.out.println("  DEVTYP: " + devType);
+              //System.out.println("  VOLSER: " + volser);
+              //System.out.println("  DEVTYP: " + devType);
             }
           }
         } catch (Exception e) {
