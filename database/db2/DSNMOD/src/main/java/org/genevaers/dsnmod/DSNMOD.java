136784eb4f06cac1a2591d1fb48c650740f680a0
neilbeesley <neil.beesley@ibm.com>
2026-05-07 00:28:58 -0500

    typo
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/DSNMOD/src/main/java/org/genevaers/dsnmod/DSNMOD.java
+++ database/db2/DSNMOD/src/main/java/org/genevaers/dsnmod/DSNMOD.java
@@ -235,7 +235,7 @@ public class DSNMOD {
                 String line;
                 while ((line = reader.readLine()) != null) {
                   byte[] byteLine = line.getBytes(codepage);
-                  Integer i;
+                  //Integer i;
                   //for ( i = 0; i < byteLine.length; i++ ) {
                   //  System.out.println(byteLine[i]);
                   //}
