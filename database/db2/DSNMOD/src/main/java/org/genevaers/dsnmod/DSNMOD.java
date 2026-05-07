eabd5bdac840786f16f5a7c8d1999fb880a5bff4
neilbeesley <neil.beesley@ibm.com>
2026-05-06 19:56:25 -0500

    add logic
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/DSNMOD/src/main/java/org/genevaers/dsnmod/DSNMOD.java
+++ database/db2/DSNMOD/src/main/java/org/genevaers/dsnmod/DSNMOD.java
@@ -18,6 +18,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
