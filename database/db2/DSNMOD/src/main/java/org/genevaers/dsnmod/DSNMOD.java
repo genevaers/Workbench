8356f2e35879f176f517c8a4928f923a1518326a
neilbeesley <neil.beesley@ibm.com>
2026-05-08 22:06:47 -0500

    typo
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/DSNMOD/src/main/java/org/genevaers/dsnmod/DSNMOD.java
+++ database/db2/DSNMOD/src/main/java/org/genevaers/dsnmod/DSNMOD.java
@@ -381,7 +381,7 @@ public class DSNMOD {
         String NameOldPad = String.format("%-10s", "\"" + schemaNameOld + "\"");
         System.out.println("NameOldPad: " + NameOldPad);
         String NameNewPad = String.format("%-10s", "\"" + schemaNameNew + "\"");
-        System.out.println("NameNewPad: " + NameOldPad);
+        System.out.println("NameNewPad: " + NameNewdPad);
 
         try {
             byte[] OldSchemaBytes = NameOldPad.getBytes(codepage);
