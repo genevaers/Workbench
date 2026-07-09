419e57d217512bc5f673b1b7036b0db8d5cafcad
AswiniV <aswini.vadlamudi@ibm.com>
2026-07-09 18:14:01 -0500

    jackson dependenices upgrade
    Signed-off-by: AswiniV <aswini.vadlamudi@ibm.com>

--- plugins/genevagui/src/com/ibm/safr/we/model/utilities/importer/CopybookImporter.java
+++ plugins/genevagui/src/com/ibm/safr/we/model/utilities/importer/CopybookImporter.java
@@ -35,9 +35,9 @@ import org.genevaers.ccb2lr.CobolCollection;
 import org.genevaers.ccb2lr.Copybook2LR;
 import org.xml.sax.SAXException;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.node.ArrayNode;
+import tools.jackson.databind.node.ObjectNode;
 import com.ibm.safr.we.constants.ActivityResult;
 import com.ibm.safr.we.constants.SAFRPersistence;
 import com.ibm.safr.we.data.transfer.LRFieldTransfer;
