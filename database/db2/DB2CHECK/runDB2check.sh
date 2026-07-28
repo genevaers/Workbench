faa8088d2fea0d9a0c67fa06074667e9675d5db0
neilbeesley <neil.beesley@ibm.com>
2026-07-28 17:41:44 -0500

    update run script
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- database/db2/DB2CHECK/runDB2check.sh
+++ database/db2/DB2CHECK/runDB2check.sh
@@ -1,2 +1,4 @@
-ARG="$1";
-java -jar target/db2check-1.0.1-jar-with-dependencies.jar $ARG;
+ARG1="$1";
+ARG2="$2";
+# echo "ARG1: $ARG1 ARG2: $ARG2";
+java -jar target/db2check-1.0.1-jar-with-dependencies.jar $ARG1 $ARG2;
